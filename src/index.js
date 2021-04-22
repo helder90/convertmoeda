37a6ce123c17f4e0369ac84099800d02ac5c3197
helder90 <48700290+helder90@users.noreply.github.com>
2021-04-22 14:15:27 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,5 +10,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+// Se você deseja começar a medir o desempenho em seu aplicativo, passe uma função
+// para registrar os resultados (por exemplo: reportWebVitals (console.log))
+// ou envie para um endpoint analítico. Saiba mais: https://bit.ly/CRA-vitals
 reportWebVitals();
